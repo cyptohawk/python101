40826a9787c370ff59d5c49ea124d054ee5d14c4
cyptohawk <60145293+cyptohawk@users.noreply.github.com>
2020-01-21 18:51:59 +0300

    Update public_ip.py

--- public_ip.py
+++ public_ip.py
@@ -4,4 +4,8 @@ url = "http://api.myip.com"
 headers = {"User-agent":"Mozilla"}
 
 req = requests.get(url,headers=headers)
-resp = req.read()
+resp = req.text
+data = json.loads(resp)
+
+print("[+] Ip",data['ip'])
+print("[+] Country",data['country'])
